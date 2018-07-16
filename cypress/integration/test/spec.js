2eb7412b14b84aa0941ee1da13eb931f9ba953a4
KJK <KJK@KJK.local>
2018-07-16 09:27:30 -0600

    updated cypress tests

--- cypress/integration/test/spec.js
+++ cypress/integration/test/spec.js
@@ -3,6 +3,6 @@ describe("E2E Test V2", () => {
         cy.visit("http://localhost:3000/");
     });
     it("Makes sure Header Appears", () => {
-        cy.get("h1").should("have.text", "Around the World!");
+        cy.get("h1").should("have.text", "Welcome to Frisbee Golf HQ");
     });
 });
